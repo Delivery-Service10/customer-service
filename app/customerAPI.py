39cca66f3b3c945a58e8a954c0e1f5ba94fa4b6b
Uyirendiren Chemen <udchemen@gmail.com>
2020-06-09 07:36:49 -0400

    Implemented basic methods for database services

--- app/customerAPI.py
+++ app/customerAPI.py
@@ -21,6 +21,48 @@ def create_customer():
     return jsonify({'message': 'New  Customer created!'})
 
 
+@customer_api.route('/customer/id/<public_id>', methods=['GET'])
+@customer_api.route('/customer/id/<public_id>/', methods=['GET'])
+def get_one_customer(public_id):
+    customer = Customer.query.filter_by(public_id=public_id).first()
+    if not customer:
+        return jsonify({'message': ' No customer found'})
+    else:
+        customer_data = helper_functions.allocate_data(customer)
+        return jsonify({'customer': customer_data})
+
+
+@customer_api.route('/customer', methods=['GET'])
+@customer_api.route('/customer/', methods=['GET'])
+def get_all_customers():
+
+    customers = Customer.query.all()
+    return jsonify({'customers': helper_functions.combine_results(customers)})
+
+
+@customer_api.route('/customer/keyword/<keyword>', methods=['GET'])
+@customer_api.route('/customer/keyword/<keyword>/', methods=['GET'])
+def get_specified_customers(keyword):
+
+    customers = Customer.query.filter(Customer.last_name.like("%" + keyword + "%"))
+    return jsonify({'customers': helper_functions.combine_results(customers)})
+
+
+@customer_api.route('/customer/<public_id>', methods=['DELETE'])
+@customer_api.route('/customer/<public_id>/', methods=['DELETE'])
+def delete_store(public_id):
+    customer = Customer.query.filter_by(public_id=public_id).first()
+    if not customer:
+        return jsonify({'message': 'Customer not found'})
+    else:
+        db.session.delete(customer)
+        db.session.commit()
+        return jsonify({'message': 'Customer deleted'})
+
+
 @customer_api.route("/")
 def hello():
     return "Welcome to the customer_service_api"
+
+
+
--- app/helper_functions.py
+++ app/helper_functions.py
@@ -1,6 +1,13 @@
 import uuid
 from models import Customer
 
+def combine_results(customers):
+    output =[]
+    for customer in customers:
+        customer_data = allocate_data(customer)
+        output.append(customer_data)
+    return output
+
 
 def populate_customer(data, hashed_password):
     new_customer = Customer(public_id=str(uuid.uuid4()),
@@ -15,3 +22,17 @@ def populate_customer(data, hashed_password):
                             country=data['country']
                       )
     return new_customer
+
+
+def allocate_data(customer):
+    customer_data = {'public_id': customer.public_id,
+                     'email': customer.email,
+                     'first_name': customer.first_name,
+                     'last_name': customer.last_name,
+                     'addressLine1': customer.addressLine1,
+                     'addressLine2': customer.addressLine2,
+                     'city': customer.city,
+                     'district': customer.district,
+                     'country': customer.country
+                  }
+    return customer_data
